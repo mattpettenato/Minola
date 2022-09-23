4d68dbf1a3de9fe1dbd2798b1d28cca6b7eee2ac
mattpettenato <mattpettenato@gmail.com>
2022-09-22 17:14:37 -0700

    test

---
+++ app/models/user.rb
@@ -0,0 +1,50 @@
+
+class User < ApplicationRecord
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
+    attr_reader :password 
+    after_initialize :ensure_session_token 
+
+    has_many :reviews,
+      foreign_key: :user_id,
+      class_name: :Review
+
+    has_many :cart_items,
+      foreign_key: :user_id,
+      class_name: :CartItem
+
+    has_many :likes,
+      foreign_key: :user_id,
+      class_name: :Like
+
+    has_many :products, 
+      through: :cart_items,
+      source: :product
+  
+    #SPIRE 
+    def self.find_by_credentials(username, password)
+      user = User.find_by(username: username)
+      return nil if user.nil?
+      user.is_password?(password) ? user : nil 
+    end
+  
+    def password=(password)
+      @password = password
+      self.password_digest = BCrypt::Password.create(password)
+    end
+  
+    def is_password?(password)
+      BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+  
+    def reset_session_token! 
+      self.session_token = SecureRandom::urlsafe_base64
+      self.save! 
+      self.session_token 
+    end 
+  
+    def ensure_session_token 
+      self.session_token ||= SecureRandom::urlsafe_base64
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "static_pages#root"
+
+  namespace :api, defaults: {format: :json} do
+    # auth routes
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy]
+
+    # resources
+    resources :cart_items, only: [:index, :destroy, :create, :update]
+    resources :products, only: [:index, :show]
+    resources :reviews, only: [:create, :index]
+    resources :likes, only: [:create, :destroy]
+
+  end
+
 end
